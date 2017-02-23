72a507950a617dc60a20334f5240f824b8d211f3
AlanDash <pemalan5@gmail.com>
2017-02-22 20:45:11 -0600

    Detalles finales

--- Practica1_1.c
+++ Practica1_1.c
@@ -17,25 +17,16 @@ char sigCar(char cad[], int *i){
 void delta(char ei[], char c[], char *tabla[], char **ef){
   int aux = 0;
   for(int i=4; i<25; i=i+4){
-    if(!strcmp(ei,tabla[i])){
+    if(!strcmp(ei,tabla[i]))
     aux = i;
-    printf("indice: %d\n", i);
   }
-  }
-  printf("caracter: %s\n", c);
 
-  if(!strcmp(c,tabla[1])){
+  if(!strcmp(c,tabla[1]))
     *ef = tabla[aux+1];
-    printf("ef: %s\n", *ef);
-  }
-  else if(!strcmp(c,tabla[2])){
+  else if(!strcmp(c,tabla[2]))
     *ef = tabla[aux+2];
-    printf("ef: %s\n", *ef);
-  }
-  else if(!strcmp(c,tabla[3])){
+  else if(!strcmp(c,tabla[3]))
     *ef = tabla[aux+3];
-    printf("ef: %s\n", *ef);
-  }
 }
 
 int edoAcep(char estado[], char *finales[]){
@@ -45,52 +36,45 @@ int edoAcep(char estado[], char *finales[]){
       res = 0;
       break;
     }
-    else{
-      continue;
-    }
   }
   return res;
 }
 
 
 int main() {
-  char *matriz[] = {" ", "a", "b", "c",         // Declaracion de matriz delta
+  char *matriz[] = {  " ", "a", "b", "c",         // Declaracion de matriz delta
                       "s0", "s1", "-", "s2",
                       "s1", "-", "s3", "-",
                       "s2", "-", "-", "-",
                       "s3", "s4", "-", "s2",
                       "s4", "-", "s5", "-",
-                      "s5", "s4", "-", "s2"};
+                      "s5", "s4", "-", "s2" };
 
-  char *finales[] = {"s2", "s3"};
+  char *finales[] = { "s2", "s3" };
   char cadena[N];
   char *estado = "s0";  // Se inicializa en s0
   int i = 0;
 
   printf("Introduce una cadena de la forma \"ab|(ab)*c\" : ");
   scanf("%s", cadena);
-  printf("Cadena: %s\n", cadena);
 
   char aux = cadena[0];   // Se inicializa en el primer caracter de la cadena
-  char car[] = {aux, '\0'};
-  printf("car: %s\n", car);
+  char car[] = { aux, '\0' };
 
   while ( (strcmp(car,"\0")) != 0 ) {   // mientras no sea fin de cadena
-    printf("Comienza while\n");
-    printf("strcmp: %d\n", strcmp(car,"\0"));
-    delta(estado, car, matriz , &estado);  // se asigna a estado el estado al que es mandado con ese caracter
-    printf("estado: %s\n", estado);
-    aux = sigCar(cadena, &i);    // Se recorre la cadena
-   char aux2[] = {aux, '\0'};
-    strcpy(car,aux2);
-    printf("car: %s\n", car);
-    if(!(strcmp(estado,"-")))  // Se compara si se llego a un estado de error
+
+    delta( estado, car, matriz , &estado );  // se asigna a estado el estado al que es mandado con ese caracter
+    aux = sigCar( cadena, &i );    // Se recorre la cadena
+    char aux2[] = { aux, '\0' };
+    strcpy( car,aux2 );
+
+    if( !(strcmp(estado,"-")) )  // Se compara si se llego a un estado de error
       break;
   }
 
   if(!(strcmp(estado,"-")))
     printf("Error\n");
-  else if(edoAcep(estado, finales))
+  else if(!edoAcep(estado, finales))
     printf("Exito\n");
   else
   printf("Error\n");
