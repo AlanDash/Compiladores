391bcfcd00522c81b1dea3850b30529334eb099d
AlanDash <pemalan5@gmail.com>
2017-02-22 18:13:23 -0600

    Función sigCar.
    Se declara y se define la función sigCar para analizar cada caracter de
    la cadena.

--- Practica1_1.c
+++ Practica1_1.c
@@ -1,2 +1,18 @@
-/* Practica 1: Diseño de un analizador lexico*/
+/* Practica 1: Diseño de un analizador lexico
+para la ER "ab|(ab)*c" */
 #include <stdio.h>
+#include <string.h>
+#define N 64
+
+char sigCar(char[], int*);
+
+char sigCar(char cad[], int* i){
+  *i = (*i + 1);
+  char c = cad[*i];
+  return c;
+}
+
+int main() {
+  
+  return 0;
+}
